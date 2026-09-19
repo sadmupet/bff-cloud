525c6c9bb9029f4855468c4a200a02dc15bde367
bruz-11 <b.ruz@duocuc.cl>
2026-09-19 07:01:40 -0300

    refactor: unificar rutas del BFF en bff.routes.js

--- src/routes/bff.routes.js
+++ src/routes/bff.routes.js
@@ -5,5 +5,7 @@ const bffController = require('../controllers/bff.controller');
 router.post('/login', bffController.login);
 router.get('/dashboard/:userId', bffController.getDashboardData);
 router.post('/transferencias', bffController.hacerTransferencia);
+router.post('/register', bffController.register);
+router.get('/dashboard/:id', bffController.getDashboardData);
 
 module.exports = router;
