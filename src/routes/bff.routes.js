f6f049f112299f0b7ea7b695d12bba1b67a1cd9b
Scott <scott@skt-t.corylvke.cs@gmail.com>
2026-09-20 01:32:10 -0300

    feat: rutas modificadas para aws

--- src/routes/bff.routes.js
+++ src/routes/bff.routes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const bffController = require('../controllers/bff.controller');
 
-router.post('/login', bffController.login);
+// Dashboard y operaciones
 router.get('/dashboard/:userId', bffController.getDashboardData);
 router.post('/transferencias', bffController.hacerTransferencia);
+
+// Gestión de usuarios
 router.post('/register', bffController.register);
-router.get('/dashboard/:id', bffController.getDashboardData);
 router.get('/usuarios/buscar', bffController.buscarUsuario);
 router.post('/contactos', bffController.crearContacto);
 
